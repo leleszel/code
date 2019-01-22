8d10279bbd1609e25ec15fb45db9156514f09a5d
Jesse A. Tov <jesse.tov@gmail.com>
2019-01-22 10:47:16 -0600

    Ready for class?

--- lec04/aquarium.py
+++ lec04/aquarium.py
@@ -1,7 +1,9 @@
 # enables methods to return their own class type:
 from __future__ import annotations
-from typing import NamedTuple, List, Optional
+
+from typing import List, Optional
 from enum import Enum, auto
+from lib230 import record, Factory
 
 
 class WaterType(Enum):
@@ -11,8 +13,18 @@ class WaterType(Enum):
     SALT = auto()
 
 
-class Fish0(NamedTuple):
-    """Represents a pet fish."""
+@record
+class Fish:
+    """Represents a pet fish.
+
+    >>> f = Fish('name', 5, 10)
+    >>> f.weight_kg
+    5
+    >>> f.age_days
+    10
+    >>> f.species
+    'unknown'
+    """
 
     name:       str
     weight_kg:  float
@@ -20,62 +32,52 @@ class Fish0(NamedTuple):
     species:    str = 'unknown'
     water_type: WaterType = WaterType.FRESH
 
-    def increment_age(self) -> Fish0:
+    def increment_age(self) -> None:
         """Adds one day to the age of this fish.
 
-        >>> fish0 = Fish0('Cleo', 0.03, 18, 'goldfish', WaterType.FRESH)
-        >>> fish0.age_days
+        >>> fish = Fish('Cleo', 0.03, 18, 'goldfish', WaterType.FRESH)
+        >>> fish.age_days
         18
-        >>> fish1 = fish0.increment_age()
-        >>> fish1.age_days
+        >>> fish.increment_age()
+        >>> fish.age_days
         19
-        >>> fish2 = fish1.increment_age()
-        >>> fish2.age_days
+        >>> fish.increment_age()
+        >>> fish.age_days
         20
-        >>> fish0.age_days
-        18
         """
-        return Fish0(self.name,
-                    self.weight_kg,
-                    self.age_days + 1,
-                    self.species,
-                    self.water_type)
-
-    # changes how the object prints in the console:
-    def __repr__(self) -> str:
-        return self.name
+        self.age_days += 1
 
 
-class Aquarium2(NamedTuple):
+@record
+class Aquarium2:
     """Represents an aquarium of two fish."""
 
-    fish1: Fish0
-    fish2: Fish0
+    fish1: Fish
+    fish2: Fish
 
-    def increment_all_ages(self) -> Aquarium2:
-        """Returns a new aquarium in which every fish is one day
-        older.
+    def increment_all_ages(self) -> None:
+        """Adds one day to the age of each fish.
 
-        >>> aq0 = Aquarium2(Fish0('Cleo', 0.03, 18, 'goldfish', WaterType.FRESH),
-        ...                 Fish0('Larry', 150, 700, 'shark', WaterType.SALT))
-        >>> (aq0.fish1.age_days, aq0.fish2.age_days)
+        >>> aq = Aquarium2(Fish('Cleo', 0.03, 18, 'goldfish', WaterType.FRESH),
+        ...                Fish('Larry', 150, 700, 'shark', WaterType.SALT))
+        >>> (aq.fish1.age_days, aq.fish2.age_days)
         (18, 700)
-        >>> aq1 = aq0.increment_all_ages()
-        >>> (aq1.fish1.age_days, aq1.fish2.age_days)
+        >>> aq.increment_all_ages()
+        >>> (aq.fish1.age_days, aq.fish2.age_days)
         (19, 701)
         """
-        return Aquarium2(self.fish1.increment_age(),
-                         self.fish2.increment_age())
+        self.fish1.increment_age()
+        self.fish2.increment_age()
 
-    def heaviest_fish(self) -> Fish0:
-        """Returns whichever fish is heaviest. Ties go to fish1.
+    def heaviest_fish(self) -> Fish:
+        """Returns whichever fish is heaviest; ties go to fish1.
 
-        >>> Aquarium2(Fish0('dory', 5), Fish0('nemo', 10)).heaviest_fish()
-        nemo
-        >>> Aquarium2(Fish0('dory', 15), Fish0('nemo', 10)).heaviest_fish()
-        dory
-        >>> Aquarium2(Fish0('dory', 10), Fish0('nemo', 10)).heaviest_fish()
-        dory
+        >>> Aquarium2(Fish('dory', 5), Fish('nemo', 10)).heaviest_fish().name
+        'nemo'
+        >>> Aquarium2(Fish('dory', 15), Fish('nemo', 10)).heaviest_fish().name
+        'dory'
+        >>> Aquarium2(Fish('dory', 10), Fish('nemo', 10)).heaviest_fish().name
+        'dory'
         """
         if self.fish1.weight_kg >= self.fish2.weight_kg:
             return self.fish1
@@ -83,42 +85,43 @@ class Aquarium2(NamedTuple):
             return self.fish2
 
     def average_weight(self) -> float:
-        """Returns the average fish weight
-        >>> Aquarium2(Fish0('dory', 5), Fish0('nemo', 10)).average_weight()
+        """Returns the average fish weight.
+
+        >>> Aquarium2(Fish('dory', 5), Fish('nemo', 10)).average_weight()
         7.5
-        >>> Aquarium2(Fish0('dory', 15), Fish0('nemo', 10)).average_weight()
+        >>> Aquarium2(Fish('dory', 15), Fish('nemo', 10)).average_weight()
         12.5
         """
         return (self.fish1.weight_kg + self.fish2.weight_kg) / 2
 
 
-class Aquarium3(NamedTuple):
+@record
+class Aquarium3:
     """Represents an aquarium of three fish."""
 
-    fish1: Fish0
-    fish2: Fish0
-    fish3: Fish0
+    fish1: Fish
+    fish2: Fish
+    fish3: Fish
 
-    def increment_all_ages(self) -> Aquarium3:
-        """Returns a new aquarium in which every fish is one day
-        older.
+    def increment_all_ages(self) -> None:
+        """Adds one day to the age of each fish.
 
-        >>> aq0 = Aquarium3(Fish0('Cleo', 0.03, 18, 'goldfish', WaterType.FRESH),
-        ...                 Fish0('Larry', 150, 700, 'shark', WaterType.SALT),
-        ...                 Fish0('Otto', 500, 45, 'goldfish?', WaterType.FRESH))
-        >>> (aq0.fish1.age_days, aq0.fish2.age_days, aq0.fish3.age_days)
+        >>> aq = Aquarium3(Fish('Cleo', 0.03, 18, 'goldfish', WaterType.FRESH),
+        ...                Fish('Larry', 150, 700, 'shark', WaterType.SALT),
+        ...                Fish('Otto', 500, 45, 'goldfish?', WaterType.FRESH))
+        >>> (aq.fish1.age_days, aq.fish2.age_days, aq.fish3.age_days)
         (18, 700, 45)
-        >>> aq1 = aq0.increment_all_ages()
-        >>> (aq1.fish1.age_days, aq1.fish2.age_days, aq1.fish3.age_days)
+        >>> aq.increment_all_ages()
+        >>> (aq.fish1.age_days, aq.fish2.age_days, aq.fish3.age_days)
         (19, 701, 46)
         """
-        return Aquarium3(self.fish1.increment_age(),
-                         self.fish2.increment_age(),
-                         self.fish3.increment_age())
+        self.fish1.increment_age()
+        self.fish2.increment_age()
+        self.fish3.increment_age()
 
-    def heaviest_fish(self) -> Fish0:
+    def heaviest_fish(self) -> Fish:
         """Returns whichever fish is heaviest. Ties go to earliest."""
-        def max_fish(f1: Fish0, f2: Fish0) -> Fish0:
+        def max_fish(f1: Fish, f2: Fish) -> Fish:
             return Aquarium2(f1, f2).heaviest_fish()
         return max_fish(self.fish1, max_fish(self.fish2, self.fish3))
 
@@ -129,62 +132,67 @@ class Aquarium3(NamedTuple):
                 + self.fish3.weight_kg) / 3
 
 
-class Fish:
-    """Represents a pet fish."""
+@record
+class Aquarium:
+    """An aquarium of any number of fish.
 
-    name:       str
-    weight_kg:  float
-    age_days:   int
-    species:    str
-    water_type: WaterType
+    >>> aq = Aquarium()
+    >>> aq.append(Fish('A', 10))
+    >>> aq.append(Fish('B', 8))
+    >>> aq.append(Fish('C', 6))
+    >>> aq.append(Fish('D', 10))
+    >>> len(aq.all_fish)
+    4
+    >>> aq.all_fish[0].weight_kg
+    10
+    """
 
-    def __init__(self,
-                 name:       str,
-                 weight_kg:  float,
-                 age_days:   int = 0,
-                 species:    str = 'unknown',
-                 water_type: WaterType = WaterType.BRACKISH) -> None:
-        self.name = name
-        self.weight_kg = weight_kg
-        self.age_days = age_days
-        self.species = species
-        self.water_type = water_type
-
-    def increment_age(self) -> None:
-        """Adds one day to the age of this fish.
-
-        >>> fish0 = Fish('Cleo', 0.03, 18, 'goldfish', WaterType.FRESH)
-        >>> fish1 = fish0
-        >>> fish0.age_days
-        18
-        >>> fish0.increment_age()
-        >>> fish0.age_days
-        19
-        >>> fish0.increment_age()
-        >>> fish0.age_days
-        20
-        >>> fish1.age_days
-        20
-        """
-        self.age_days += 1
-
-    # changes how the object prints in the console:
-    def __repr__(self) -> str:
-        return self.name
-
-
-class Aquarium(NamedTuple):
-    """An aquarium of any number of fish."""
-    all_fish: List[Fish] = []
+    all_fish: List[Fish] = Factory(list)
 
     def append(self, fish: Fish) -> None:
+        """Adds a new fish to the aquarium
+
+        >>> aq = Aquarium()
+        >>> len(aq.all_fish)
+        0
+        >>> aq.append(Fish('A', 1))
+        >>> aq.append(Fish('B', 2))
+        >>> len(aq.all_fish)
+        2
+        >>> aq.all_fish[1].name
+        'B'
+        """
         self.all_fish.append(fish)
 
     def increment_all_ages(self) -> None:
+        """Increases the age of every fish in the aquarium by
+        one day.
+
+        >>> aq = Aquarium([ Fish('A', 1, 10),
+        ...                 Fish('B', 2, 20),
+        ...                 Fish('C', 3, 30) ])
+        >>> aq.increment_all_ages()
+        >>> aq.all_fish[0].age_days
+        11
+        """
         for fish in self.all_fish:
             fish.increment_age()
 
     def heaviest_fish(self) -> Optional[Fish]:
+        """Finds the heaviest fish, if any.
+
+        >>> aq = Aquarium()
+        >>> aq.heaviest_fish()
+        >>> aq.append(Fish('A', 10))
+        >>> aq.heaviest_fish().name
+        'A'
+        >>> aq.append(Fish('B', 15))
+        >>> aq.heaviest_fish().name
+        'B'
+        >>> aq.append(Fish('C', 15))
+        >>> aq.heaviest_fish().name
+        'B'
+        """
         best = None
         for fish in self.all_fish:
             if best is None or fish.weight_kg > best.weight_kg:
