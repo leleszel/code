20ef049d7617103683705054a6e7e6f20b159bb9
Jesse A. Tov <jesse.tov@gmail.com>
2019-01-22 13:55:50 -0600

    End of class today.

--- lec04/data_definitions.py
+++ lec04/data_definitions.py
@@ -10,11 +10,16 @@ Each class should have a purpose statement, and maybe some doctests.
 
 
 from enum import Enum, auto
-from typing import Optional
+from typing import Optional, NamedTuple
 from lib230 import record
 
 
 # Example 1: car share ride
+
+
+class Person2(NamedTuple):
+    id:     int
+    name:   str
 
 
 @record
@@ -31,6 +36,16 @@ class Location:
 
 @record
 class Ride:
+    """Represents a car-share ride.
+
+    >>> Ride(3, 4, 5, 6)
+    Ride(passenger=3, driver=4, start=5, end=6)
+    >>> ride = Ride(3, 4, 5, 6)
+    >>> ride.passenger
+    3
+    >>> ride.end
+    6
+    """
     passenger:  Person
     driver:     Person
     start:      Location
@@ -47,7 +62,27 @@ class CardinalDirection(Enum):
     WEST = auto()
 
 
+def is_north_or_south(cd: CardinalDirection) -> bool:
+    return (cd is CardinalDirection.NORTH
+            or cd is CardinalDirection.SOUTH)
+
+
+def is_multiple_of_3_or_5(n: int) -> bool:
+    b3_ = n % 3 == 0
+    b5_ = n % 5 == 0
+    return b3_ or b5_
+
+
 # Example 3: flowering plants
+
+
+# Every plant has:
+#  - name
+#  - maximum height
+#  - sun preference, which is one of:
+#     - shade
+#     - partial
+#     - full
 
 
 class SunPreference(Enum):
@@ -58,33 +93,17 @@ class SunPreference(Enum):
 
 @record
 class FloweringPlant:
-    common_name:    str
-    max_height:     float
+    name: str
+    max_height: float
     sun_preference: SunPreference
 
 
+def okay_for_under_ledge(audrey2: FloweringPlant) -> bool:
+    return audrey2.max_height < 10 and \
+           audrey2.sun_preference is not SunPreference.FULL
+
+
 # Example 4: electronic medical records
-
-
-@record
-class BloodLipids:
-    hdl_cholesterol: float
-    ldl_cholesterol: float
-    triglycerides:   float
-
-
-@record
-class BloodPressure:
-    systolic:        float
-    diastolic:       float
-
-
-@record
-class Vitals:
-    blood_pressure:  BloodPressure
-    body_tempature:  float
-    heart_rate:      int
-    breath_rate:     int
 
 
 class Sex(Enum):
@@ -102,11 +121,25 @@ class Biometrics:
 
 
 @record
+class BloodPressure:
+    systolic:  float
+    diastolic: float
+
+
+@record
+class Vitals:
+    body_temperature: float
+    blood_pressure: BloodPressure
+    heart_rate: int
+    breath_rate: int
+
+
+@record
 class MedicalRecord:
     id:             int
     name:           str
     biometrics:     Biometrics
-    vitals:         Vitals
-    blood_lipids:   Optional[BloodLipids]
+    vitals:         Optional[Vitals]
+
 
 
