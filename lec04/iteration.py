50de0602df7dd60c5bc9dac6cbb46d24c4ea9a09
Jesse A. Tov <jesse.tov@gmail.com>
2019-01-24 13:08:23 -0600

    LIVE UPDATE

--- lec04/iteration.py
+++ lec04/iteration.py
@@ -1,4 +1,4 @@
-"""our first loop!
+"""Our first loop!
 
 This file demonstates two design strategies:
 
@@ -12,17 +12,19 @@ from typing import Optional
 from typing import Callable, Iterable
 
 
+# "Helper function" for substr_index.
+def _matches_at(needle: str, haystack: str, base: int) -> bool:
+    if len(needle) + base > len(haystack):
+        return False
+    for i in range(len(needle)):
+        if needle[i] != haystack[base + i]:
+            return False
+    return True
+
+
 def substr_index(needle: str, haystack: str) -> Optional[int]:
-    """Returns the first index in `haystack` where `needle` may be
-    found, or None if `needle` is not a substring of `haystack`.
+    for i in range(len(haystack)):
+        if _matches_at(needle, haystack, i):
+            return i
+    return None
 
-    >>> substr_index('a', 'apple')
-    0
-    >>> substr_index('l', 'apple')
-    3
-    >>> substr_index('g', 'apple')
-    >>> substr_index('pl', 'apple')
-    2
-    """
-
-    3
--- lec04/test_iteration.py
+++ lec04/test_iteration.py
@@ -1,4 +1,5 @@
-from iteration import _matches_at, str_index, str_index2
+from iteration import _matches_at, substr_index
+# , substr_index2
 
 
 def test__matches_at():
@@ -13,25 +14,25 @@ def test__matches_at():
     assert(_matches_at('', 'apple', 5))
 
 
-def test_str_index():
-    assert(str_index('a', 'apple') == 0)
-    assert(str_index('b', 'apple') is None)
-    assert(str_index('p', 'apple') == 1)
-    assert(str_index('l', 'apple') == 3)
-    assert(str_index('pp', 'apple') == 1)
-    assert(str_index('ppp', 'apple') is None)
-    assert(str_index('pl', 'apple') == 2)
-    assert(str_index('hello', 'h') is None)
-    assert(str_index('', 'apple') == 0)
+def test_substr_index():
+    assert(substr_index('a', 'apple') == 0)
+    assert(substr_index('b', 'apple') is None)
+    assert(substr_index('p', 'apple') == 1)
+    assert(substr_index('l', 'apple') == 3)
+    assert(substr_index('pp', 'apple') == 1)
+    assert(substr_index('ppp', 'apple') is None)
+    assert(substr_index('pl', 'apple') == 2)
+    assert(substr_index('hello', 'h') is None)
+    assert(substr_index('', 'apple') == 0)
 
 
-def test_str_index2():
-    assert(str_index2('a', 'apple') == 0)
-    assert(str_index2('b', 'apple') is None)
-    assert(str_index2('p', 'apple') == 1)
-    assert(str_index2('l', 'apple') == 3)
-    assert(str_index2('pp', 'apple') == 1)
-    assert(str_index2('ppp', 'apple') is None)
-    assert(str_index2('pl', 'apple') == 2)
-    assert(str_index2('hello', 'h') is None)
-    assert(str_index2('', 'apple') == 0)
+# def test_substr_index2():
+#     assert(substr_index2('a', 'apple') == 0)
+#     assert(substr_index2('b', 'apple') is None)
+#     assert(substr_index2('p', 'apple') == 1)
+#     assert(substr_index2('l', 'apple') == 3)
+#     assert(substr_index2('pp', 'apple') == 1)
+#     assert(substr_index2('ppp', 'apple') is None)
+#     assert(substr_index2('pl', 'apple') == 2)
+#     assert(substr_index2('hello', 'h') is None)
+#     assert(substr_index2('', 'apple') == 0)
